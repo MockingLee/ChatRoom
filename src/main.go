b3a4cfa36fd111316a95b4d5318f0ffb5b56dd27
MockingLee <38075220+MockingLee@users.noreply.github.com>
2019-06-25 19:45:11 +0800

    main web server finish

--- src/main.go
+++ src/main.go
@@ -9,11 +9,15 @@ func sayHello(writer http.ResponseWriter , request *http.Request){
 	request.ParseForm()
 	fmt.Println("receive request")
 	fmt.Println("method : " , request.Method)
-	fmt.Println()
 	fmt.Println(request.Form)
+	fmt.Fprintln(writer , "hello")
 }
 
 func main() {
 	fmt.Println("Starting web server...")
-	http.HandleFunc()
+	http.HandleFunc("/",sayHello)
+	err := http.ListenAndServe("0.0.0.0:9999" , nil)
+	if err == nil{
+		fmt.Println(err)
+	}
 }
