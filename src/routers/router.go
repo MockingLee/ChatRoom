a65099ce365582e838474191a4aea3753a5eca56
MockingLee <38075220+MockingLee@users.noreply.github.com>
2019-06-27 08:23:12 +0800

    tmp commit

--- src/routers/router.go
+++ src/routers/router.go
@@ -1,12 +1,14 @@
 package routers
 
 import (
-	"github.com/astaxie/beego"
 	"../controllers"
+	"github.com/astaxie/beego"
 )
 
 func init() {
     beego.Router("/", &controllers.MainController{})
     beego.Router("/test" , &controllers.ApiController{})
     beego.Router("/api/userLogin" , &controllers.UserLoginControllers{})
+    beego.Router("/api/userin" , &controllers.WebSocketController{})
+    beego.Router("/api/ws",&controllers.WebSocketController{} , "get:Join")
 }
